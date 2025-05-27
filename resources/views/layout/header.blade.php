73162f4d8a6e30aabe6377fa5379aa2febc6e73c
Rpebs pramesta <rpebspramesta@gmail.com>
2025-05-27 13:49:08 +0700

    Update header view to include meta tags for description, keywords, author, and favicon

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -3,6 +3,10 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Undangan Silaturahmi Keluarga Bani Sakirin & Sama'un" />
+    <meta name="keywords" content="Undangan, Silaturahmi, Keluarga, Bani Sakirin, Sama'un" />
+    <meta name="author" content="Ramadhany" />
+    <link rel="icon" href="{{ asset('img/favicon.ico') }}" />
     <title>Undangan Silaturahmi Keluarga Bani Sakirin & Sama'un</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
   </head>
