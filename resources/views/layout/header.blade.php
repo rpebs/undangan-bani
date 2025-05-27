8608252885575b07b8d60088638a7cd1597e62dc
Rpebs pramesta <rpebspramesta@gmail.com>
2025-05-27 16:05:54 +0700

    Fix favicon path in header layout

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -6,7 +6,7 @@
     <meta name="description" content="Undangan Silaturahmi Keluarga Bani Sakirin & Sama'un" />
     <meta name="keywords" content="Undangan, Silaturahmi, Keluarga, Bani Sakirin, Sama'un" />
     <meta name="author" content="Ramadhany" />
-    <link rel="icon" href="{{ asset('img/favicon.ico') }}" />
+    <link rel="icon" href="{{ asset('favicon.ico') }}" />
     <title>Undangan Silaturahmi Keluarga Bani Sakirin & Sama'un</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
   </head>
